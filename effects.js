0eed6601b7700cb8927a76fc04498223ebda05a0
Unknown <simon.kok@live.dk>
2019-06-20 11:08:54 +0200

    Moving SoMe buttons

---
+++ effects.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const fb_icon = document.querySelector("#fb_icon");
+  fb_icon.onmouseover = function() {mouseOver(fb_icon)};
+  fb_icon.onmouseout = function() {mouseOut(fb_icon)};
+
+  const linkedin_icon = document.querySelector("#linkedin_icon");
+  linkedin_icon.onmouseover = function() {mouseOver(linkedin_icon)};
+  linkedin_icon.onmouseout = function() {mouseOut(linkedin_icon)};
+
+  alert("Dom is load");
+});
+
+function mouseOver(icon) {
+  icon.style.bottom = "25px";
+}
+
+function mouseOut(icon) {
+  icon.style.bottom = "20px";
+}
