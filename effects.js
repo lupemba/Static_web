4cbe3f2ae401500cca1b1d875d287765ce393724
Unknown <simon.kok@live.dk>
2019-06-20 17:11:11 +0200

    Crazy button effects

--- effects.js
+++ effects.js
@@ -2,17 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const fb_icon = document.querySelector("#fb_icon");
   fb_icon.onmouseover = function() {mouseOver(fb_icon)};
-  fb_icon.onmouseout = function() {mouseOut(fb_icon)};
 
   const linkedin_icon = document.querySelector("#linkedin_icon");
   linkedin_icon.onmouseover = function() {mouseOver(linkedin_icon)};
-  linkedin_icon.onmouseout = function() {mouseOut(linkedin_icon)};
 });
 
-function mouseOver(icon) {
-  icon.style.bottom = "25px";
-}
+let direction = 1;
 
-function mouseOut(icon) {
-  icon.style.bottom = "20px";
+function mouseOver(icon) {
+  // Get the value of the buttom
+  const elem_style = getComputedStyle(icon);
+  let integer = parseInt(elem_style.bottom, 10);
+  // Check If it should move up or down
+  if (integer>400) {
+    direction = -1;
+  }
+  if (integer<50) {
+    direction = 1;
+  }
+  //move icon
+  icon.style.bottom = (integer+direction*40)+"px";
 }
